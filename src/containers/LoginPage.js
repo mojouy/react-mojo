4a7b69e20c47ed3af97a375702bfa045dcf32fdb
mojouy <santiago.mollajoli@neonroots.com>
2018-02-09 15:19:15 -0300

    Fix warning on login, related to issue 44

--- src/containers/LoginPage.js
+++ src/containers/LoginPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
@@ -8,18 +8,24 @@ import LoginForm from '../components/session/LoginForm'; // eslint-disable-line 
 import { routes } from '../constants/routesPaths';
 import SharedFormLink from '../components/common/SharedFormLink';
 
-const LoginPage = ({ actions: { login }, authenticated }) => {
-  if (authenticated) {
-    browserHistory.push(routes.index);
+class LoginPage extends Component {
+  componentWillReceiveProps(nextProps) {
+    const { authenticated } = nextProps;
+    if (authenticated) {
+      browserHistory.push(routes.index);
+    }
   }
-  return (
-    <div>
-      <p>LOGIN</p>
-      <LoginForm onSubmit={login} />
-      <SharedFormLink name="Sign up" to={routes.signUp} />
-    </div>
-  );
-};
+  render() {
+    const { actions: { login } } = this.props;
+    return (
+      <div>
+        <p>LOGIN</p>
+        <LoginForm onSubmit={login} />
+        <SharedFormLink name="Sign up" to={routes.signUp} />
+      </div>
+    );
+  }
+}
 
 const { bool, object } = PropTypes;
 
